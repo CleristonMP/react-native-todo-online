b28f4f3321787b63ffed6d5e16a32d1636e645ab
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2023-05-09 19:21:31 -0300

    Project created and first screen

---
+++ src/screens/TaskList.tsx
@@ -0,0 +1,58 @@
+import React, {Component} from 'react';
+import {View, Text, StyleSheet, ImageBackground} from 'react-native';
+
+import commonStyles from '../commonStyles';
+
+import 'moment/locale/pt-br';
+import moment from 'moment';
+
+export default class TaskList extends Component {
+  render() {
+    const today = moment().locale('pt-br').format('ddd, D [de] MMMM');
+    return (
+      <View style={styles.container}>
+        <ImageBackground
+          source={require('../../assets/imgs/today.jpg')}
+          style={styles.background}>
+          <View style={styles.titleBar}>
+            <Text style={styles.title}>Hoje</Text>
+            <Text style={styles.subtitle}>{today}</Text>
+          </View>
+        </ImageBackground>
+        <View style={styles.taskList}>
+          <Text>TaskList</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  background: {
+    flex: 3,
+  },
+  taskList: {
+    flex: 7,
+  },
+  titleBar: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  title: {
+    fontFamily: commonStyles.fontFamily,
+    color: commonStyles.colors.secondary,
+    fontSize: 50,
+    marginLeft: 20,
+    marginBottom: 20,
+  },
+  subtitle: {
+    fontFamily: commonStyles.fontFamily,
+    color: commonStyles.colors.secondary,
+    fontSize: 20,
+    marginLeft: 20,
+    marginBottom: 30,
+  },
+});
