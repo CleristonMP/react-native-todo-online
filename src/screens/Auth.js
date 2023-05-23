ec2cb71b103d30bc2b2354bd29ac5d4afa040266
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2023-05-23 10:59:39 -0300

    SignUp layout

--- src/screens/Auth.js
+++ src/screens/Auth.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {
+  Alert,
   ImageBackground,
   Platform,
   StyleSheet,
@@ -14,8 +15,19 @@ import commonStyles from '../commonStyles';
 
 export default class Auth extends Component {
   state = {
+    name: '',
     email: '',
     password: '',
+    confirmPassword: '',
+    stageNew: false,
+  };
+
+  signInOrsignUp = () => {
+    if (this.state.stageNew) {
+      Alert.alert('Sucesso!', 'Criar conta');
+    } else {
+      Alert.alert('Sucesso!', 'Logar');
+    }
   };
 
   render() {
@@ -23,6 +35,17 @@ export default class Auth extends Component {
       <ImageBackground source={bckgImg} style={styles.background}>
         <Text style={styles.title}>Tasks</Text>
         <View style={styles.formContainer}>
+          <Text style={styles.subtitle}>
+            {this.state.stageNew ? 'Crie a sua conta' : 'Informe seus dados'}
+          </Text>
+          {this.state.stageNew && (
+            <TextInput
+              placeholder="Nome"
+              value={this.state.name}
+              style={styles.input}
+              onChangeText={name => this.setState({name})}
+            />
+          )}
           <TextInput
             placeholder="E-mail"
             value={this.state.email}
@@ -34,13 +57,34 @@ export default class Auth extends Component {
             value={this.state.password}
             style={styles.input}
             onChangeText={password => this.setState({password})}
+            secureTextEntry={true}
           />
-          <TouchableOpacity>
+          {this.state.stageNew && (
+            <TextInput
+              placeholder="Confirmação de senha"
+              value={this.state.confirmPassword}
+              style={styles.input}
+              onChangeText={confirmPassword => this.setState({confirmPassword})}
+              secureTextEntry={true}
+            />
+          )}
+          <TouchableOpacity onPress={this.signInOrsignUp}>
             <View style={styles.button}>
-              <Text style={styles.buttonText}>Entrar</Text>
+              <Text style={styles.buttonText}>
+                {this.state.stageNew ? 'Registrar' : 'Entrar'}
+              </Text>
             </View>
           </TouchableOpacity>
         </View>
+        <TouchableOpacity
+          style={styles.p10}
+          onPress={() => this.setState({stageNew: !this.state.stageNew})}>
+          <Text style={styles.buttonText}>
+            {this.state.stageNew
+              ? 'Já possui conta?'
+              : 'Ainda não possui conta?'}
+          </Text>
+        </TouchableOpacity>
       </ImageBackground>
     );
   }
@@ -57,6 +101,13 @@ const styles = StyleSheet.create({
     fontFamily: commonStyles.fontFamily,
     color: commonStyles.colors.secondary,
     fontSize: 70,
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontFamily: commonStyles.fontFamily,
+    color: '#FFF',
+    fontSize: 20,
+    textAlign: 'center',
     marginBottom: 10,
   },
   formContainer: {
@@ -79,5 +130,8 @@ const styles = StyleSheet.create({
     fontFamily: commonStyles.fontFamily,
     color: '#FFF',
     fontSize: 20,
+  },
+  p10: {
+    padding: 10,
   },
 });
