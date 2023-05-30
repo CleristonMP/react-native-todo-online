066e17aa24e9ab1f9d1d477e3cf3b9bd064c9072
Cleriston Pereira <cleriston.melo.pereira@gmail.com>
2023-05-30 12:00:56 -0300

    Drawer navigator config

--- src/Navigator.tsx
+++ src/Navigator.tsx
@@ -1,7 +1,33 @@
+import React from 'react';
 import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 
 import Auth from './screens/Auth';
 import TaskList from './screens/TaskList';
+import {NavigationContainer} from '@react-navigation/native';
+
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Hoje">
+        <Drawer.Screen name="Hoje">
+          {(props: any) => <TaskList title="Hoje" daysAhead={0} {...props} />}
+        </Drawer.Screen>
+        <Drawer.Screen name="Amanhã">
+          {(props: any) => <TaskList title="Amanhã" daysAhead={1} {...props} />}
+        </Drawer.Screen>
+        <Drawer.Screen name="Semana">
+          {(props: any) => <TaskList title="Semana" daysAhead={7} {...props} />}
+        </Drawer.Screen>
+        <Drawer.Screen name="Mês">
+          {(props: any) => <TaskList title="Mês" daysAhead={30} {...props} />}
+        </Drawer.Screen>
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
 
 const mainRoutes = {
   Auth: {
@@ -10,7 +36,7 @@ const mainRoutes = {
   },
   Home: {
     name: 'Home',
-    screen: TaskList,
+    screen: MyDrawer,
   },
 };
 
